78ce874ebcec1c4a6320c46313ffde5c0a359fc2
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 00:57:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(attendees)
   attendees.collect do |attendee| 
     # room_num = attendees.index(attendee) + 1
-    "Hello #{attendee}! You'll be assigned to room #{attendees.index(attendee) + 1}!"
+    "Hello, #{attendee}! You'll be assigned to room #{attendees.index(attendee) + 1}!"
   end
 end
 
